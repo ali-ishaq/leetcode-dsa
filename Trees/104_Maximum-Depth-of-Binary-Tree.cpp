ee18fb74f0734c342ea327f38f40c612d144de1c
Ali-Ishaq <aliishaq3578@gmail.com>
2026-08-28 23:55:28 +0500

    LC#104 recursive-dfs solution

--- Trees/104_Maximum-Depth-of-Binary-Tree.cpp
+++ Trees/104_Maximum-Depth-of-Binary-Tree.cpp
@@ -1,15 +1,4 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
+// BFS Approach
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
@@ -34,3 +23,14 @@ public:
         return depth;
     }
 };
+
+// Recursive DFS Approach
+class Solution {
+public:
+ int maxDepth(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+        return 1+ max(maxDepth(root->left),maxDepth(root->right));
+    }
+}
